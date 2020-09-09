b91cb31c77349fdff26ba3ff00da7a57fceebba6
JeffMorhous <jobboy18@gmail.com>
2020-09-08 21:53:06 -0400

    Initial Commit

---
+++ HWSProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWSProject1
+//
+//  Created by Jeff Morhous on 9/8/20.
+//  Copyright © 2020 Jeff Morhous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
